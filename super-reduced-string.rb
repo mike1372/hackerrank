34512f59aae12914a3bd48a381e73ebe3a3663ac
Michael Kempster <michaelkempster@bigpond.com>
2016-08-30 22:38:23 +1000

    Initial commit.

---
+++ super-reduced-string.rb
@@ -0,0 +1,16 @@
+input = gets.chomp
+
+counter = 0
+flag = false
+while counter < input.length
+    flag = false
+    if input[counter + 1]
+        if input[counter] == input[counter + 1]
+            flag = true
+            2.times { input.sub!(input[counter], '') }
+        end
+    end
+    flag ? counter = 0 : counter += 1
+end
+
+puts input.empty? ? 'Empty String' : input
